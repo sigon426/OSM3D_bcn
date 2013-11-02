f9fc2a480fa07993cd98d992962845efcffabd52
mappingCo <mapping.and.co@gmail.com>
2013-11-02 23:36:46 +0100

    update stats in main.js

--- js/main.js
+++ js/main.js
@@ -61,11 +61,15 @@
 
 
   //stats 
-  stats = new Stats();
+  var stats = new Stats();
+  stats.setMode(1); // 0: fps, 1: ms
+
+  // Align top-left
   stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
   stats.domElement.style.top = '0px';
   stats.domElement.style.zIndex = 100;
-  container.appendChild( stats.domElement );
+
 
 
   scene.add(light);
