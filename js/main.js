4de146c0c33b72b8e1022f0537ce5c20903b6d06
mappingCo <mapping.and.co@gmail.com>
2013-11-04 20:20:59 +0100

    change camera position

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@
   
   // the camera starts at 0,0,0 (change x y z....)
   camera.position.y = 1500;
-  camera.position.x = 700;
+  camera.position.x = 500;
 
   // add the camera to the scene
   scene.add(camera);
