9dc86485038a998c0abd8806c0aeb1af9ac4426b
mappingCo <mapping.and.co@gmail.com>
2013-12-19 22:35:17 +0100

    change all materials color to 5131854

--- obj/mapas/map2.js
+++ obj/mapas/map2.js
@@ -16,181 +16,181 @@
     "scale" : 1.000000,
 
     "materials": [	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 0,
 	"DbgName" : "ASPHALT_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 1,
 	"DbgName" : "KERB_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 2,
 	"DbgName" : "MAT_0_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 3,
 	"DbgName" : "BUS_STOP_SIGN_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 4,
 	"DbgName" : "STEEL_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 5,
 	"DbgName" : "ROAD_MARKING_DASHED_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 6,
 	"DbgName" : "WOOD_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 7,
 	"DbgName" : "COBBLESTONE_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 8,
 	"DbgName" : "RED_ROAD_MARKING_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 9,
 	"DbgName" : "GRAVEL_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 10,
 	"DbgName" : "MAT_1_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 11,
 	"DbgName" : "MAT_2_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 12,
 	"DbgName" : "TUNNEL_DEFAULT_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 13,
 	"DbgName" : "RAIL_BALLAST_DEFAULT_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 14,
 	"DbgName" : "RAIL_DEFAULT_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 15,
 	"DbgName" : "BRIDGE_DEFAULT_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 16,
 	"DbgName" : "STEPS_DEFAULT_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 17,
 	"DbgName" : "MAT_4_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 18,
 	"DbgName" : "CONCRETE_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 19,
 	"DbgName" : "FENCE_DEFAULT_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 20,
 	"DbgName" : "WALL_DEFAULT_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 21,
 	"DbgName" : "PAVING_STONE_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 22,
 	"DbgName" : "TERRAIN_DEFAULT_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 23,
 	"DbgName" : "WATER_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 24,
 	"DbgName" : "SAND_0"
 	},
 
 	{
-	"DbgColor" : 0x00ee00,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 25,
 	"DbgName" : "BUILDING_DEFAULT_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 26,
 	"DbgName" : "ROOF_DEFAULT_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 27,
 	"DbgName" : "EARTH_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 28,
 	"DbgName" : "GRASS_0"
 	},
 
 	{
-	"DbgColor" : 9686361,
+	"DbgColor" : 5131854,
 	"DbgIndex" : 29,
 	"DbgName" : "PURIFIED_WATER_0"
 	}],
